592e14047978e9384ce46e0111900ea12b7914e5
Abhinav Verma <abhinavverma@outlook.in>
2025-01-15 21:08:41 +0530

    c8

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -19,14 +19,12 @@ const io=new Server(server,{
 });
 
 io.on("connection",(socket)=>{
-    console.log('A user connected',socket.id);
     const userId = socket.handshake.query.userId;
     if(userId) userSocketMap[userId]=socket.id;
 
     io.emit('getOnlineUsers',Object.keys(userSocketMap));
 
     socket.on('disconnect',()=>{
-        console.log("a user disconnected",socket.id);
         delete userSocketMap[userId];
         io.emit('getOnlineUsers',Object.keys(userSocketMap));
     })
